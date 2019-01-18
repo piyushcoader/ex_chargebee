3d659064269e84d07891fb5696df63b77ae9a17e
devchain 007 <p@wscode.info>
2019-01-18 14:05:29 +0545

    used alias in subscription

--- lib/module/subscription.ex
+++ lib/module/subscription.ex
@@ -2,11 +2,11 @@ defmodule ChargeBee.Module.Subscription do
   alias ChargeBee.Endpoint
   alias ChargeBee.Utils
   @url_suffix "/subscriptions"
-  
+
   def fetch_all(), do: Endpoint.request([], @url_suffix, :get)
 
   def create(params) when is_map(params) do
-    ChargeBee.Utils.string_map_to_atom(params)
+    Utils.string_map_to_atom(params)
     |> Endpoint.request(@url_suffix, :post)
 
   end
