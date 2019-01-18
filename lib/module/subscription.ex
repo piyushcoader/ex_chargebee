5532e7c6b8c9495536e8c5bd9106a4df1553626c
devchain 007 <p@wscode.info>
2019-01-18 14:02:00 +0545

    changed subscription to handle new Endpoint method

--- lib/module/subscription.ex
+++ lib/module/subscription.ex
@@ -1,16 +1,14 @@
 defmodule ChargeBee.Module.Subscription do
-alias ChargeBee.Endpoint
-
-  def fetch_all() do
-    url_suffix = "/subscriptions"
-    Endpoint.request(:get, url_suffix, [])
-  end
+  alias ChargeBee.Endpoint
+  alias ChargeBee.Utils
+  @url_suffix "/subscriptions"
+  
+  def fetch_all(), do: Endpoint.request([], @url_suffix, :get)
 
   def create(params) when is_map(params) do
-    url_suffix = "/subscriptions"
-    params = ChargeBee.Utils.string_map_to_atom(params)
-    
-    Endpoint.request(:post, url_suffix, params)
+    ChargeBee.Utils.string_map_to_atom(params)
+    |> Endpoint.request(@url_suffix, :post)
+
   end
 
   def create(_), do: {:error, %{reason: "Invalid Parameter"}}
