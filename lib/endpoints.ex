24ee7fce8b943fa440ec519063a33f9f3973c9cc
devchain 007 <p@wscode.info>
2019-01-18 14:20:48 +0545

    fixed parameter mismatch

--- lib/endpoints.ex
+++ lib/endpoints.ex
@@ -1,17 +1,17 @@
 defmodule ChargeBee.Endpoint do
   use HTTPoison.Base
-  @options %{ protocol: "https", host_suffix: ".chargebee.com", api_path: "/api/v2", timeout: 40_000, version: 'v2.4.7', port: 443}
+  @options %{ protocol: "https", host_suffix: ".chargebee.com", api_path: "api/v2", timeout: 40_000, version: 'v2.4.7', port: 443}
 
   @api_key Application.get_env(:ex_chargebee, :api_key)
   @site Application.get_env(:ex_chargebee, :site)
 
 
-  def request(params \\ %{} , method \\ :post , url_suffix) do
+  def request(params \\ [] , url_suffix ,  method \\ :post) do
 
-    url = "#{@options.protocol}://#{@site}#{@options.host_suffix}#{@options.api_path}#{url_suffix}"
-
+    url = "https://#{@site}.chargebee.com/#{@options.api_path}#{url_suffix}"
     token = :base64.encode("#{@api_key}:")
-    body = process_request_body(params)
+    body =  process_request_body(params)
+    
     headers = [
       {"Authorization" ,  "Basic #{token}"},
       {"Accept",  "application/json"},
@@ -21,12 +21,12 @@ defmodule ChargeBee.Endpoint do
     result =
       case HTTPoison.request(method, url, body , headers, []) do
         {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
-
-        {:ok, parsed_data} =  Jason.decode(body)
-        Map.put(parsed_data, "http_status_code", status_code)
+          {:ok, parsed_data} =  Jason.decode(body)
+          Map.put(parsed_data, "http_status_code", status_code)
 
         {:error, reason} ->
           IO.inspect reason
+          reason
       end
 
     if result["http_status_code"] != 200, do: {:error, result}, else: {:ok, result}
