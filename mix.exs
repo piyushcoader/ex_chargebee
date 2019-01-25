e88a1baffadbb6383bdbd13b00893cc01c881bfb
piyushcoader <piyushthapa74@gmail.com>
2019-01-26 01:49:58 +0545

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChargeBee.MixProject do
   def project do
     [
       app: :ex_chargebee,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
